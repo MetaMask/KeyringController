b703ea8c757d19e69a69656a96964b1307c84971
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2020-03-20 14:20:26 -0700

    Fix unlock event emission (#60)
    * only emit unlock when password submitted, after keyrings available in memory

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ class KeyringController extends EventEmitter {
    * randomly creates a new HD wallet with 1 account,
    * faucets that account on the testnet.
    *
+   * @emits KeyringController#unlock
    * @param {string} password - The password to encrypt the vault with.
    * @returns {Promise<Object>} A Promise that resolves to the state.
    */
@@ -71,6 +72,7 @@ class KeyringController extends EventEmitter {
     return this.persistAllKeyrings(password)
       .then(this.createFirstKeyTree.bind(this))
       .then(this.persistAllKeyrings.bind(this, password))
+      .then(this.setUnlocked.bind(this))
       .then(this.fullUpdate.bind(this))
   }
 
@@ -81,6 +83,7 @@ class KeyringController extends EventEmitter {
    * creates a new encrypted store with the given password,
    * creates a new HD wallet from the given seed with 1 account.
    *
+   * @emits KeyringController#unlock
    * @param {string} password - The password to encrypt the vault with
    * @param {string} seed - The BIP44-compliant seed phrase.
    * @returns {Promise<Object>} A Promise that resolves to the state.
@@ -113,6 +116,7 @@ class KeyringController extends EventEmitter {
         return null
       })
       .then(this.persistAllKeyrings.bind(this, password))
+      .then(this.setUnlocked.bind(this))
       .then(this.fullUpdate.bind(this))
   }
 
@@ -140,6 +144,8 @@ class KeyringController extends EventEmitter {
    *
    * Temporarily also migrates any old-style vaults first, as well.
    * (Pre MetaMask 3.0.0)
+   *
+   * @emits KeyringController#unlock
    * @param {string} password - The keyring controller password.
    * @returns {Promise<Object>} A Promise that resolves to the state.
    */
@@ -147,6 +153,7 @@ class KeyringController extends EventEmitter {
     return this.unlockKeyrings(password)
       .then((keyrings) => {
         this.keyrings = keyrings
+        this.setUnlocked()
         return this.fullUpdate()
       })
   }
@@ -500,7 +507,6 @@ class KeyringController extends EventEmitter {
     }
 
     this.password = password
-    this.setUnlocked()
     return Promise.all(this.keyrings.map((keyring) => {
       return Promise.all([keyring.type, keyring.serialize()])
         .then((serializedKeyringArray) => {
@@ -538,7 +544,6 @@ class KeyringController extends EventEmitter {
     await this.clearKeyrings()
     const vault = await this.encryptor.decrypt(password, encryptedVault)
     this.password = password
-    this.setUnlocked()
     await Promise.all(vault.map(this.restoreKeyring.bind(this)))
     return this.keyrings
   }
