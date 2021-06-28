c80808b28972f149de1c3d1675b463b9c895ad60
Mark Stacey <markjstacey@gmail.com>
2021-06-28 12:07:15 -0230

    Update `mocha` from v7 to v9
    `mocha` has been updated to v9. The breaking changes made for v8 and
    v9 don't affect us [1][2]. Most notably they include dropping support
    for Node.js v10.
    
    The "unhandled rejection" handler setup in the test helper was removed,
    as this is now default behaviour in `mocha` as of v8.2.0 [3].
    
    [1]: https://github.com/mochajs/mocha/releases/tag/v9.0.0
    [2]: https://github.com/mochajs/mocha/releases/tag/v8.0.0
    [3]: https://github.com/mochajs/mocha/issues/2640#issuecomment-769646345

--- test/helper.js
+++ test/helper.js
@@ -1,6 +1,3 @@
-// disallow promises from swallowing errors
-enableFailureOnUnhandledPromiseRejection()
-
 const getRandomValuesPoly = require('polyfill-crypto.getrandomvalues')
 
 //
@@ -23,34 +20,4 @@ if (!window.crypto) {
 
 if (!window.crypto.getRandomValues) {
   window.crypto.getRandomValues = getRandomValuesPoly
-}
-
-function enableFailureOnUnhandledPromiseRejection (...args) {
-  // modified from https://github.com/mochajs/mocha/issues/1926#issuecomment-180842722
-
-  // rethrow unhandledRejections
-  if (typeof process !== 'undefined') {
-    process.on('unhandledRejection', function (reason) {
-      throw reason
-    })
-  } else if (typeof window !== 'undefined') {
-    // 2016-02-01: No browsers support this natively, however bluebird, when.js,
-    // and probably other libraries do.
-    if (typeof window.addEventListener === 'function') {
-      window.addEventListener('unhandledrejection', function (evt) {
-        throw evt.detail.reason
-      })
-    } else {
-      const oldOHR = window.onunhandledrejection
-      window.onunhandledrejection = function (evt) {
-        if (typeof oldOHR === 'function') {
-          oldOHR.apply(this, args)
-        }
-        throw evt.detail.reason
-      }
-    }
-  } else if (typeof console !== 'undefined' &&
-      typeof (console.error || console.log) === 'function') {
-    (console.error || console.log)('Unhandled rejections will be ignored!')
-  }
 }
