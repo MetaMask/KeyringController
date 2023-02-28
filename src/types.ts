46bc491c5e565e1933477d15afcf00169d69c5b2
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2023-02-27 23:13:08 -0300

    Update types

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,10 @@
 import type { Hex, Eip1024EncryptedData, Keyring } from '@metamask/utils';
 
-export type IKeyringController = {};
+export type IKeyringController = any;
 
 export type MessageParams = {
   from: Hex | string;
-  data: Hex | string | Eip1024EncryptedData;
+  data: Hex | string | Eip1024EncryptedData | Record<string, unknown>[];
 };
 
 export enum KeyringType {
