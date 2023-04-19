c79f1ebd86c5afb031c54a338939172c1f5e1d50
Daniel Rocha <daniel@rocha.io>
2023-04-19 15:13:08 +0200

    chore: add `Vault` to `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { KeyringController, keyringBuilderFactory } from './KeyringController';
 
 export { KeyringType, KeyringControllerError } from './constants';
+
+export { Vault } from './Vault';
