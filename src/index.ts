3b96ebfd6d2e8e9dacae9af7fec8a0c3b8c6ac05
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2023-04-23 22:03:01 -0400

    Add new exports to index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
-export { KeyringController, keyringBuilderFactory } from './KeyringController';
+export {
+  KeyringController,
+  keyringBuilderFactory,
+  controllerName,
+} from './KeyringController';
+
+export type {
+  KeyringControllerState,
+  KeyringControllerEvents,
+  KeyringControllerActions,
+  KeyringControllerMessenger,
+  KeyringControllerArgs,
+  // Events
+  AccountRegistered,
+  AccountRemoved,
+  KeyringRegistered,
+  KeyringRemoved,
+  VaultLocked,
+  VaultUnlocked,
+  VaultCreated,
+  StateUpdated,
+  // Actions
+  AddAccount,
+  GetAccount,
+  UpdateAccount,
+  ListAccounts,
+  RemoveAccount,
+} from './KeyringController';
 
 export { KeyringType, KeyringControllerError } from './constants';
